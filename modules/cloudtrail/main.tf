7d5840f714cb5b3fb1535c638f7d75f2cec8418a
Warren Lee <warren.lee@dyedurham.com>
2025-04-11 12:24:30 +0100

    Update logging

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -1,5 +1,9 @@
 resource "aws_cloudtrail" "insecure-logging" {
   name           = "cloudtrail-logging"
   s3_bucket_name = "my-cloudtrail-bucket"
-  enable_logging = false
+  enable_logging = true
+  enable_log_file_validation = true
+  kms_key_id = new-key-id-tbc
+  is_multi_region_trail = true
+  cloud_watch_logs_group_arn = new-log-group-tbc
 }
